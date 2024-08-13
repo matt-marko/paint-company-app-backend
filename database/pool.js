275b93ef3fe586a967e323da5380b219b3cc8831
Matt Marko <mattmarko321@gmail.com>
2024-08-13 11:19:34 -0700

    Set up dependencias

--- database/pool.js
+++ database/pool.js
@@ -1,10 +1,14 @@
 const pg = require('pg');
+const dotenv = require('dotenv');
+
+const envFile = process.env.NODE_ENV === 'prod' ? '../.env.prod' : '../.env.dev';
+dotenv.config({ path: envFile });
  
 const pool = new pg.Pool({
-  user: 'postgres',
-  host: 'localhost',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
   database: 'postgres',
-  password: 'password',
+  password: process.env.DB_PASSWORD,
   port: 5432,
 });
 
